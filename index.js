3b7dec06d9c8501531d12b0104570a3c2a0f1b6d
2Kelvin <rocketmancodes02@gmail.com>
2025-10-27 12:56:37 +0300

    add register button

--- index.js
+++ index.js
@@ -102,6 +102,8 @@ const pages = [
     { text: 'Blog', url: './pages/blog.html' },
     { text: 'Gallery', url: './pages/gallery.html' },
     { text: 'About Us', url: './pages/aboutUs.html' },
+    // replace this with link to google form
+    { text: 'Register', url: './pages/aboutUs.html' },
 ];
 
 function createNavLinks(navElement) {
@@ -110,6 +112,10 @@ function createNavLinks(navElement) {
         link.textContent = page.text;
         link.href = page.url;
         link.classList.add('navPageLink');
+        // styling the register button differently
+        if (page.text == 'Register') {
+            link.classList.add('registerButton');
+        }
         link.addEventListener('click', () => {
             // Close menu when link is clicked
             navigationPages.classList.remove('navIsOpen');
